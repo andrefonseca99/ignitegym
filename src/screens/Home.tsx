6d1aa6d5c3f077993d1e5f572586df7f16659a7d
André Pereira Fonseca <andrepfonseca19@gmail.com>
2023-01-30 09:25:59 -0300

    Creating and using Group component on Home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,33 @@
+import { useState } from 'react';
+import { VStack, FlatList } from 'native-base';
+import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
-import { VStack } from 'native-base';
 
 export function Home(){
+
+    const [ groups, setGroups ] = useState(["costa", "Biceps", "Triceps", "Ombro"]);
+    const [ groupSelected, setGroupSelected ] = useState("costa");
+
     return(
         <VStack flex={1}>
             <HomeHeader/>
+
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({item }) =>(
+                    <Group
+                    name={item}
+                    isActive={groupSelected === item}
+                    onPress={() => setGroupSelected(item)}
+                />
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                _contentContainerStyle={{ px:8 }}
+                my={10}
+                maxH={10}
+            />
         </VStack>
     );
 }
