19aa0b27f14a62d00f6ab82337ab391875b6a021
André Pereira Fonseca <andrepfonseca19@gmail.com>
2023-02-04 20:58:38 -0300

    Using state on context

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
 
 import { UserDTO } from "@dtos/UserDTO";
 
@@ -13,15 +13,16 @@ type AuthContextProviderProps = {
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children } : AuthContextProviderProps){
+
+    const [user, setUser] = useState({
+        id: '1',
+        name: 'Andre',
+        email: 'andre@email.com',
+        avatar: 'andre.png'
+    });
+
     return(
-        <AuthContext.Provider value={{
-            user: {
-              id: '1',
-              name: 'Andre',
-              email: 'andre@email.com',
-              avatar: 'andre.png'
-            }
-          }}>
+        <AuthContext.Provider value={{ user }}>
             { children }
         </AuthContext.Provider>
     );
